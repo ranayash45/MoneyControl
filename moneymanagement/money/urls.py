50585c2fd65cd6532e8a8e1213a2a94966be2175
Yash Rana <y.rana18@outlook.com>
2022-06-10 23:20:53 +0530

    Add and Update Expense Functionality Added

--- moneymanagement/money/urls.py
+++ moneymanagement/money/urls.py
@@ -7,5 +7,8 @@ urlpatterns=[
     path("list/",views.IndexView.as_view(),name="list"),
     path("details/<int:expense_id>",views.DetailView.as_view()),
     path("users/",views.users),
-    path("add/",views.AddExpense,name='add')
+    path("add/",views.AddExpense,name='add'),
+    path("update/<int:expense_id>",views.UpdateExpense,name='update'),
+    path("update/",views.UpdateExpense,name='update'),
+    path("delete/<int:expense_id>",views.deleteExpense,name='delete')
 ]
--- moneymanagement/money/views.py
+++ moneymanagement/money/views.py
@@ -1,3 +1,5 @@
+from cmath import exp
+from datetime import datetime
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from .models import Expense
 from django.template import loader
@@ -8,9 +10,19 @@ from django.views import generic
 
 class IndexView(generic.ListView):
     template_name = "index.html"
-    context_object_name = "expense_list"
+    context_object_name = "expense"
     def get_queryset(self):
-        return Expense.objects.all()
+        expense = {
+            "list":[],
+            "total":0
+        }
+        total = 0
+        expense_list = Expense.objects.all()
+        for item in expense_list:
+            total += item.expenseValue
+        expense["list"] = expense_list
+        expense["total"] = total
+        return expense
 
 class DetailView(generic.DetailView):
     template_name = "detail.html"
@@ -25,10 +37,31 @@ def AddExpense(request):
         expenseName = request.POST["expenseName"]
         expenseCost = request.POST["expenseCost"]
         e = Expense(expenseName=expenseName,expenseValue=expenseCost,expenseDate=timezone.now())
-        
         e.save()
         return HttpResponseRedirect("/money/list")
     return render(request,"add.html")
 
 def users(request):
     return HttpResponse("Total Users")
+
+def deleteExpense(request,expense_id):
+    expense = get_object_or_404(Expense,pk=expense_id)
+    expense.delete()
+    return HttpResponseRedirect("/money/list")
+
+def UpdateExpense(request,expense_id=""):
+    if request.method == "POST":
+        expense_id = request.POST["expense_id"]
+        expense_name = request.POST["expenseName"]
+        expense_value = request.POST["expenseValue"]
+        expense = get_object_or_404(Expense,pk=expense_id)
+        expense.expenseName = expense_name
+        expense.expenseValue = expense_value
+        expense.save()
+        return HttpResponseRedirect("/money/list")
+    else:
+        expense = get_object_or_404(Expense,pk=expense_id)
+        context = {
+            'expense':expense
+        }
+        return render(request,'update.html',context)
